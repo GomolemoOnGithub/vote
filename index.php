6b879070664b02baad9435bbfd1f162512781339
mo5e5 <emailmemoshe@gmail.com>
2021-02-19 21:20:50 +0200

    commeting and fixing error messages

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ session_start();
 
 function __autoload($classname)
 {
-    include 'includes/class.' . $classname . '.inc';
+  include 'includes/class.' . $classname . '.inc';
 }
 
 // Class Objects
@@ -18,60 +18,58 @@ $page = 'Audience vote';
 ?>
 <!DOCTYPE html>
 <html lang="e-us">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <meta name="description" content="">
-    <meta http-equiv="imagetoolbar" content="no" />
-    <meta name="Moses Ditlhong" content="Miss Botswana 2019">
-    <link rel="icon" type="image/x-icon" href="images/favicon.ico">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title><?php echo $textitle ?></title>
+  <meta name="description" content="">
+  <meta http-equiv="imagetoolbar" content="no" />
+  <meta name="Moses Ditlhong" content="Miss Botswana 2019">
+  <link rel="icon" type="image/x-icon" href="images/favicon.ico">
 
-    <script src="https://code.jquery.com/jquery-2.2.4.js"
-  integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI="
-  crossorigin="anonymous"></script>
+  <title><?php echo $textitle ?></title>
+
+  <script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
 
 
-    <!-- bulma -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">
+  <!-- bulma -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">
 
-    <!-- Custome Style Sheet   -->
-    <link rel="stylesheet" href="css/app.css">
+  <!-- Custome Style Sheet   -->
+  <link rel="stylesheet" href="css/app.css">
 
-    <script defer src="https://use.fontawesome.com/releases/v5.1.0/js/all.js"></script>
+  <script defer src="https://use.fontawesome.com/releases/v5.1.0/js/all.js"></script>
 
-    <script src="js/app.js"></script>
+  <script src="js/app.js"></script>
 
-    <!-- Sweet Alert -->
-    <script src="https://unpkg.com/sweetalert2@7.8.2/dist/sweetalert2.all.js"></script>
+  <!-- Sweet Alert -->
+  <script src="https://unpkg.com/sweetalert2@7.8.2/dist/sweetalert2.all.js"></script>
 </head>
 
-  <body>
-    <div style="font-family: https://fonts.google.com/specimen/Nunito?selection.family=Nunito"   class="section is-paddingless">
-            <div class="section" >
-              <div class="section has-background-warning">
-                <p class="title ">
-                  Miss Botswana 2019
-                </p>
-                <p class="subtitle"><?php echo $page; ?></p>
-                 <div class="field">
-                    <div class="control">
-                      <input id="ticket_code" class="input is-medium is-fullwidth has-text-centered" type="text" placeholder="Enter ticket code.">
-                    </div>
-                </div>
+<body>
+  <div style="font-family: https://fonts.google.com/specimen/Nunito?selection.family=Nunito" class="section is-paddingless">
+    <div class="section">
+      <div class="section has-background-warning">
+        <p class="title ">
+          Poll Title
+        </p>
+        <p class="subtitle"><?php echo $page; ?></p>
+        <div class="field">
+          <div class="control">
+            <input id="ticket_code" class="input is-medium is-fullwidth has-text-centered" type="text" placeholder="Enter ticket code.">
+          </div>
+        </div>
       </div>
-        <br>
+      <br>
 
+      <?php echo $candidate->getCandidates(); ?>
 
-    <?php echo $candidate->getCandidates(); ?>
+      <div class="field">
+        <div class="control ">
+          <a id="results" href="./results.php" class="button is-success has-background-dark has-text-grey-light is-large is-fullwidth is-radiusless">Results</a>
+        </div>
+      </div>
 
-    <div class="field">
-    <div class="control ">
-    <a id="results" href="./results.php" class="button is-success has-background-dark has-text-grey-light is-large is-fullwidth is-radiusless" ">Results</a>
     </div>
-    </div>
-
-</div>
-<?php include "includes/layouts/footer.php";?>
+    <?php include "includes/layouts/footer.php"; ?>
--- js/app.js
+++ js/app.js
@@ -1,131 +1,90 @@
 // Add module
-$(document).ready(function(){
+$(document).ready(function () {
+  document
+    .getElementById("ticket_code")
+    .addEventListener("input", function (e) {
+      e.target.value = e.target.value
+        .replace(/[^\dA-Z]/g, "")
+        .replace(/(.{2})/g, "$1 ")
+        .trim();
+    });
 
-
-document.getElementById('ticket_code').addEventListener('input', function (e) {
-  e.target.value = e.target.value.replace(/[^\dA-Z]/g, '').replace(/(.{2})/g, '$1 ').trim();
-});
-
-
-// remove notification
-$("#dtbtn").click(function(){
+  // remove notification
+  $("#dtbtn").click(function () {
     $("#note").hide();
+  });
+
+  //submit ticket
+
+  $(".vote").click(function () {
+    //inputs
+    var $ticket_code = $("#ticket_code");
+    var $candidate = $(this);
+
+    if ($ticket_code.val() == "") {
+      swal("Ticket Code?", "Please enter your ticket Code..", "info");
+    } else {
+      // json object
+      var ticket = {
+        ticket_code: $ticket_code.val().replace(/\s/g, ""),
+      };
+
+      $.ajax({
+        type: "POST",
+        url: "includes/ajax/check.php",
+        data: ticket,
+        cache: false,
+        success: function (result) {
+          if (result == "0") {
+            swal(
+              "Ticket Code?",
+              "You entered an invalid ticket number, try again",
+              "info"
+            );
+          } else {
+            swal({
+              title: $.trim($candidate.text()),
+              text: "is your vote?",
+              type: "info",
+              confirmButtonColor: "#3085d6",
+              cancelButtonColor: "#B3B3B3",
+              showCancelButton: true,
+              confirmButtonText: "Yes",
+              cancelButtonText: "No",
+            }).then((result) => {
+              if (result.value) {
+                var vote = {
+                  ticket_code: $ticket_code.val().replace(/\s/g, ""),
+                  candidate_id: $candidate.attr("id"),
+                  candidate_name: $candidate.text(),
+                };
+
+                $.ajax({
+                  type: "POST",
+                  url: "includes/ajax/add_vote.php",
+                  data: vote,
+                  cache: false,
+                  success: function (result) {
+                    if (result == 1) {
+                      swal(
+                        "Well done!",
+                        "Vote Successful",
+                        "success"
+                      );
+                    } else {
+                      swal(
+                        "Ticket already used",
+                        "One ticket, one vote",
+                        "info"
+                      );
+                    }
+                  },
+                });
+              }
+            });
+          }
+        },
+      });
+    }
+  });
 });
-
-
-//submit ticket
-
-$('.vote').click(function(){
-
-//inputs
-var $ticket_code         = $('#ticket_code');
-var $candidate         = $(this);
-
-
-if ( $ticket_code.val() == '') {
-
-swal("Ticket Code?", "Please enter your ticket Code..", "info");
-
-}
-else
-
-{
-
-
-// json object
-var ticket = {
-ticket_code     : $ticket_code.val().replace(/\s/g,''),
-}
-
-$.ajax({
-type: 'POST',
-url: 'includes/ajax/check.php',
-data: ticket,
-cache : false,
-success: function(result){
-
-if(result == '0')
-
-{
-swal("Ticket Code?", "You entered an invalid ticket number, try again",'info');
-}
-
-else
-
-{
-
-swal({
-title: $.trim($candidate.text()),
-text: "is your vote?" ,
-type: 'info',
-confirmButtonColor: '#3085d6',
-cancelButtonColor: '#B3B3B3',
-showCancelButton: true,
-confirmButtonText: 'Yes',
-cancelButtonText: 'No',
-
-}
-
-).then((result) => {
-
-if (result.value) {
-
-
-// //ip address
-// var $x;
-
-// //get ip address
-// $.getJSON('http://ip.jsontest.com/?callback=?', function(data) {
-
-//   $x = data
-
-// });
-
-// json object
-var vote = {
-ticket_code     : $ticket_code.val().replace(/\s/g,'') ,
-candidate_id    : $candidate.attr('id'),
-candidate_name  : $candidate.text() ,
-// ip_addresss     : $x.ip ,
-}
-
-$.ajax({
-type: 'POST',
-url: 'includes/ajax/add_vote.php',
-data: vote,
-cache : false,
-success: function(result){
-
-if(result ==  1)
-
-{
-swal(
-'Well done!',
-'We appreciate you being part of this journey to uncover our 2018 queen.',
-'success'
-);
-}
-else
-{
-
-swal("Ticket already used", "One ticket, one vote",'info');
-}
-
-}
-
-});
-}
-})
-
-}
-}
-});
-
-}
-});
-
-});
-
-
-
-
